f45992a82ee865057087e9dd949c7b66edb3c2d3
TatsumiMizubayashi <112037683+TatsumiMizubayashi@users.noreply.github.com>
2023-08-31 11:18:15 +0900

    OptionBase作成

---
+++ PolyploidQtlSeqCore/Options/OptionBase.cs
@@ -0,0 +1,70 @@
+﻿namespace PolyploidQtlSeqCore.Options
+{
+    /// <summary>
+    /// オプションの抽象クラス
+    /// </summary>
+    public abstract class OptionBase
+    {
+        /// <summary>
+        /// 該当するLongOptionNameを取得する。
+        /// 該当しない場合は空文字を返す。
+        /// </summary>
+        /// <param name="name">名前</param>
+        /// <returns>LongName</returns>
+        public string GetLongName(string name)
+        {
+            if (name == GetShortName()) return GetLongName();
+            if (name == GetLongName()) return GetLongName();
+
+            return "";
+        }
+
+        /// <summary>
+        /// パラメータ辞書から値を設定する。
+        /// 該当する情報が辞書に無い場合や、オプションスイッチでユーザー指定されている場合は何もしない。
+        /// </summary>
+        /// <param name="longNameParameterDictionary">LongNameパラメーター辞書</param>
+        /// <param name="userSetLongNameDictionary">ユーザー指定オプションLongName辞書</param>
+        public void SetValue(IReadOnlyDictionary<string, string> longNameParameterDictionary,
+            IReadOnlyDictionary<string, bool> userSetLongNameDictionary)
+        {
+            if (userSetLongNameDictionary.ContainsKey(GetLongName())) return;
+            if (!longNameParameterDictionary.TryGetValue(GetLongName(), out var value)) return;
+
+            SetValue(value);
+        }
+
+        /// <summary>
+        /// Key-Value文字列を取得する。
+        /// </summary>
+        /// <returns>Key-Value</returns>
+        public string GetKeyValue()
+        {
+            return $"{GetLongName()}\t{GetStringValue()}";
+        }
+
+        /// <summary>
+        /// オプションスイッチShortNameを取得する。
+        /// </summary>
+        /// <returns>ShortName</returns>
+        protected abstract string GetShortName();
+
+        /// <summary>
+        /// オプションスイッチLongNameを取得する。
+        /// </summary>
+        /// <returns>LongName</returns>
+        protected abstract string GetLongName();
+
+        /// <summary>
+        /// 値を設定する。
+        /// </summary>
+        /// <param name="value">文字列値</param>
+        protected abstract void SetValue(string value);
+
+        /// <summary>
+        /// 値の文字列値を取得する。
+        /// </summary>
+        /// <returns>文字列値</returns>
+        protected abstract string GetStringValue();
+    }
+}
