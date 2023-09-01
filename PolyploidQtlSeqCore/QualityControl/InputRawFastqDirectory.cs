65edf05d1199f323abb77143e5199f12e35b6f0b
TatsumiMizubayashi <112037683+TatsumiMizubayashi@users.noreply.github.com>
2023-09-01 11:29:13 +0900

    InputRawFastqDirectory修正
    引数１個のコンストラクタを追加

--- PolyploidQtlSeqCore/QualityControl/InputRawFastqDirectory.cs
+++ PolyploidQtlSeqCore/QualityControl/InputRawFastqDirectory.cs
@@ -43,6 +43,18 @@ namespace PolyploidQtlSeqCore.QualityControl
         }
 
         /// <summary>
+        /// 入力RawFastqディレクトリを作成する。
+        /// </summary>
+        /// <param name="inputDirPath">入力RawFastqディレクトリPath</param>
+        public InputRawFastqDirectory(string inputDirPath)
+        {
+            if (string.IsNullOrEmpty(inputDirPath)) throw new ArgumentException(null, nameof(inputDirPath));
+            if (!Directory.Exists(inputDirPath)) throw new DirectoryNotFoundException($"{inputDirPath} not found.");
+
+            Path = System.IO.Path.GetFullPath(inputDirPath);
+        }
+
+        /// <summary>
         /// 入力RawFastqディレクトリのPATHを取得する。
         /// </summary>
         internal string Path { get; }
