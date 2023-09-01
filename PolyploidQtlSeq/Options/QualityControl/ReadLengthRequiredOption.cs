9e7017f8bb1a93021bbce92fea35190942bb5fbe
TatsumiMizubayashi <112037683+TatsumiMizubayashi@users.noreply.github.com>
2023-09-01 09:45:46 +0900

    ReadLengthRequiredOption作成

---
+++ PolyploidQtlSeq/Options/QualityControl/ReadLengthRequiredOption.cs
@@ -0,0 +1,68 @@
+﻿using PolyploidQtlSeqCore.Application.QualityControl;
+using PolyploidQtlSeqCore.Options;
+
+namespace PolyploidQtlSeq.Options.QualityControl
+{
+    /// <summary>
+    /// 必須リード長オプション
+    /// </summary>
+    internal class ReadLengthRequiredOption : Option
+    {
+        /// <summary>
+        /// オプションスイッチのShortName
+        /// </summary>
+        public const string SHORT_NAME = "l";
+
+        /// <summary>
+        /// オプションスイッチのLongName
+        /// </summary>
+        public const string LONG_NAME = "lengthRequired";
+
+        /// <summary>
+        /// オプションスイッチの説明
+        /// </summary>
+        public const string DESCRIPTION = "Minimum read length after trimming. length_required in fastp.";
+
+        /// <summary>
+        /// リード最低長の規定値
+        /// </summary>
+        public const int DEFAULT = 50;
+
+        /// <summary>
+        /// リード最低長の最小値
+        /// </summary>
+        private const int MINIMUM = 10;
+
+        /// <summary>
+        /// リード最低長の最大値
+        /// </summary>
+        private const int MAXIMUM = 300;
+
+        private readonly IFastpQualityControlOptionValue _optionValue;
+
+        /// <summary>
+        /// 必須リード長オプション インスタンスを作成する。
+        /// </summary>
+        /// <param name="optionValue">Fastq QCオプション値</param>
+        public ReadLengthRequiredOption(IFastpQualityControlOptionValue optionValue)
+        {
+            _optionValue = optionValue;
+        }
+
+        public override DataValidationResult Validation()
+        {
+            if (MINIMUM <= _optionValue.ReadLengthRequired && _optionValue.ReadLengthRequired <= MAXIMUM) return new DataValidationResult();
+
+            return new DataValidationResult(SHORT_NAME, LONG_NAME, 
+                $"Minimum read length must be an integer greater than {MINIMUM} and less than or equal to {MAXIMUM}.");
+        }
+
+        protected override string GetLongName() => LONG_NAME;
+
+        protected override string GetShortName() => SHORT_NAME;
+
+        protected override string GetStringValue() => _optionValue.ReadLengthRequired.ToString();
+
+        protected override void SetValue(string value) => _optionValue.ReadLengthRequired = int.Parse(value);
+    }
+}
--- PolyploidQtlSeqCore/QualityControl/ReadLengthRequired.cs
+++ PolyploidQtlSeqCore/QualityControl/ReadLengthRequired.cs
@@ -10,36 +10,43 @@ namespace PolyploidQtlSeqCore.QualityControl
         /// <summary>
         /// リード最低長の最小値
         /// </summary>
+        [Obsolete("削除予定")]
         public const int MINIMUM = 10;
 
         /// <summary>
         /// リード最低長の最大値
         /// </summary>
+        [Obsolete("削除予定")]
         public const int MAXIMUM = 150;
 
         /// <summary>
         /// リード最低長の規定値
         /// </summary>
+        [Obsolete("削除予定")]
         public const int DEFAULT = 50;
 
         /// <summary>
         /// オプションスイッチのShortName
         /// </summary>
+        [Obsolete("削除予定")]
         public const string SHORT_NAME = "l";
 
         /// <summary>
         /// オプションスイッチのLongName
         /// </summary>
+        [Obsolete("削除予定")]
         public const string LONG_NAME = "lengthRequired";
 
         /// <summary>
         /// オプションスイッチの説明
         /// </summary>
+        [Obsolete("削除予定")]
         public const string DESCRIPTION = "Minimum lead length after trimming. length_required in fastp.";
 
         /// <summary>
         /// データ検証エラーメッセージ
         /// </summary>
+        [Obsolete("削除予定")]
         public const string VALIDATION_ERROR_MESSAGE = "The -l option must be an integer greater than 10 and less than or equal to 150.";
 
 
@@ -49,6 +56,7 @@ namespace PolyploidQtlSeqCore.QualityControl
         /// <param name="length">リード最低長</param>
         /// <param name="parameterDictionary">パラメータファイルの中身</param>
         /// <param name="userOptionDictionary">ユーザー指定LongName辞書</param>
+        [Obsolete("削除予定")]
         public ReadLengthRequired(int length, IReadOnlyDictionary<string, string> parameterDictionary,
             IReadOnlyDictionary<string, bool> userOptionDictionary)
         {
@@ -75,6 +83,7 @@ namespace PolyploidQtlSeqCore.QualityControl
         /// パラメータファイル記載用行テキストに変換する。
         /// </summary>
         /// <returns>パラメータ行テキスト</returns>
+        [Obsolete("削除予定")]
         public string ToParameterFileLine()
         {
             return $"{LONG_NAME}\t{Value}";
