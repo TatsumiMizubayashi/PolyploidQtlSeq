fd40bc3b7776e18c5ef3480151dd67ab0ebcd491
TatsumiMizubayashi <112037683+TatsumiMizubayashi@users.noreply.github.com>
2023-09-01 11:14:05 +0900

    FastpQualityControlOptions作成

---
+++ PolyploidQtlSeq/Options/QualityControl/FastpQualityControlOptions.cs
@@ -0,0 +1,41 @@
+﻿using PolyploidQtlSeqCore.Application.QualityControl;
+using PolyploidQtlSeqCore.Options;
+
+namespace PolyploidQtlSeq.Options.QualityControl
+{
+    /// <summary>
+    /// Fastp QCオプション
+    /// </summary>
+    internal class FastpQualityControlOptions : OptionCollection
+    {
+        /// <summary>
+        /// Fastp QCオプションインスタンスを作成する。
+        /// </summary>
+        /// <param name="options">オプション</param>
+        private FastpQualityControlOptions(Option[] options) : base(options)
+        {
+        }
+
+        /// <summary>
+        /// Fastp QCオプションインスタンスを作成する。
+        /// </summary>
+        /// <param name="optionValue">Fastp QCオプション値</param>
+        /// <returns>Fastp QCオプション</returns>
+        public static FastpQualityControlOptions Create(IFastpQualityControlOptionValue optionValue)
+        {
+            var options = new Option[]
+            {
+                new InputRawFastqDirectoryOption(optionValue),
+                new OutputFastqDirectoryOption(optionValue),
+                new ReadLengthRequiredOption(optionValue),
+                new NBaseLimitOption(optionValue),
+                new BaseQualityOption(optionValue),
+                new CutTailMeanQualityOption(optionValue),
+                new CutTailWindowSizeOption(optionValue),
+                new ThreadNumberOption(optionValue),
+            };
+
+            return new FastpQualityControlOptions(options);
+        }
+    }
+}
