b0d16dadbcde268474b42ae39041e9f8678a217b
TatsumiMizubayashi <112037683+TatsumiMizubayashi@users.noreply.github.com>
2023-09-04 14:05:46 +0900

    Parent2SnpIndexRange修正
    引数1個のコンストラクタを追加

--- PolyploidQtlSeqCore/QtlAnalysis/QtlSeqTargetFilter/Parent2SnpIndexRange.cs
+++ PolyploidQtlSeqCore/QtlAnalysis/QtlSeqTargetFilter/Parent2SnpIndexRange.cs
@@ -12,12 +12,12 @@ namespace PolyploidQtlSeqCore.QtlAnalysis.QtlSeqTargetFilter
         /// <summary>
         /// SNP-indexの最小値
         /// </summary>
-        public const double MINIMUM = 0;
+        private const double MINIMUM = 0;
 
         /// <summary>
         /// SNP-indexの最大値
         /// </summary>
-        public const double MAXIMUM = 1.0;
+        private const double MAXIMUM = 1.0;
 
         /// <summary>
         /// Parent2 SNP-index範囲の規定値
@@ -58,6 +58,33 @@ namespace PolyploidQtlSeqCore.QtlAnalysis.QtlSeqTargetFilter
             if (string.IsNullOrWhiteSpace(value)) throw new ArgumentException(null, nameof(range));
 
             var items = value.Split(_splitter);
+            if (items.Length != 2) throw new ArgumentException("Specify by lower limit - upper limit.", nameof(range));
+
+            if (!double.TryParse(items[0], out var lower))
+                throw new ArgumentException("The lower limit connot bo converted to a numerical value.", nameof(range));
+            if (!double.TryParse(items[1], out var upper))
+                throw new ArgumentException("The upper limit cannot be converted to a numerical value.", nameof(range));
+
+            if (lower < MINIMUM || lower > MAXIMUM)
+                throw new ArgumentException("The lower limit should be specified in the range of 0.0 to 1.0.", nameof(range));
+            if (upper < MINIMUM || upper > MAXIMUM)
+                throw new ArgumentException("The upper limit should be specified in the range of 0.0 to 1.0.", nameof(range));
+            if (lower == upper) throw new ArgumentException("The lower and upper limits are the same value.", nameof(range));
+            if (lower > upper) throw new ArgumentException("The lower limit is greater than the upper limit.", nameof(range));
+
+            Lower = lower;
+            Upper = upper;
+        }
+
+        /// <summary>
+        /// Parent2 SNP-index範囲
+        /// </summary>
+        /// <param name="range">SNP-index範囲</param>
+        public Parent2SnpIndexRange(string range)
+        {
+            if (string.IsNullOrWhiteSpace(range)) throw new ArgumentException(null, nameof(range));
+
+            var items = range.Split(_splitter);
             if (items.Length != 2) throw new ArgumentException("Specify by lower limit - upper limit.", nameof(range));
 
             if (!double.TryParse(items[0], out var lower))
