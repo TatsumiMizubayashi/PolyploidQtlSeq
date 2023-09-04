548ff62cf400939f6686569229e9cbee8a527c61
TatsumiMizubayashi <112037683+TatsumiMizubayashi@users.noreply.github.com>
2023-09-04 14:44:58 +0900

    DisplayAnnotationImpacts修正
    引数１個のコンストラクタを追加

--- PolyploidQtlSeqCore/QtlAnalysis/IO/DisplayAnnotationImpacts.cs
+++ PolyploidQtlSeqCore/QtlAnalysis/IO/DisplayAnnotationImpacts.cs
@@ -66,6 +66,29 @@ namespace PolyploidQtlSeqCore.QtlAnalysis.IO
         }
 
         /// <summary>
+        /// 表示するアノテーションImpactを作成する。
+        /// </summary>
+        /// <param name="impacts">表示するImpact</param>
+        public DisplayAnnotationImpacts(string impacts)
+        {
+            if (string.IsNullOrEmpty(impacts)) throw new ArgumentException(null, nameof(impacts));
+
+            Impacts = impacts;
+            
+            var items = Impacts.Split(_splitter);
+            var flag = Impact.None;
+            foreach (var item in items)
+            {
+                if (!Enum.TryParse<Impact>(item, out var impact))
+                    throw new ArgumentException($"{item} is an invalid value.");
+
+                flag |= impact;
+            }
+
+            DisplayFlag = flag;
+        }
+
+        /// <summary>
         /// 表示するImapctsのテキストを取得する。
         /// </summary>
         private string Impacts { get; }
