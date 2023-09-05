94a253e0a2c2efcab5fae5a1cfa72e6a712662c4
TatsumiMizubayashi <112037683+TatsumiMizubayashi@users.noreply.github.com>
2023-09-05 11:54:14 +0900

    IQtlSeqPipelineOptionValue作成

---
+++ PolyploidQtlSeq/Options/Pipeline/IQtlSeqPipelineOptionValue.cs
@@ -0,0 +1,62 @@
+﻿using PolyploidQtlSeq.Options.QtlAnalysis;
+
+namespace PolyploidQtlSeq.Options.Pipeline
+{
+    /// <summary>
+    /// QTL-Seqパイプラインオプション値 インターフェース
+    /// </summary>
+    internal interface IQtlSeqPipelineOptionValue : IQtlSeqAnalysisOptionValue
+    {
+        public string ReferenceSequence { get; set; }
+
+        public string Parent1Dir { get; set; }
+
+        public string Parent2Dir { get; set; }
+
+        public string Bulk1Dir { get; set; }
+
+        public string Bulk2Dir { get; set; }
+
+        public int ChrSizeThreshold { get; set; }
+
+        public string AnalysisChrNames { get; set; }
+
+        public int MinMq { get; set; }
+
+        public int MinBq { get; set; }
+
+        public int AdjustMq { get; set; }
+
+        public int SnpEffMaxHeap { get; set; }
+
+        public string SnpEffConfigFile { get; set; }
+
+        public string SnpEffDatabaseName { get; set; }
+
+        /// <summary>
+        /// VariantCallPipelineSettingValueを作成する。
+        /// </summary>
+        /// <returns>VariantCallPipelineSettingValue</returns>
+        internal VariantCallPipelineSettingValueDTO CreateVariantCallPipelineSettingValue()
+        {
+            return new VariantCallPipelineSettingValueDTO()
+            {
+                ReferenceSequence = ReferenceSequence,
+                Parent1Dir = Parent1Dir,
+                Parent2Dir = Parent2Dir,
+                Bulk1Dir = Bulk1Dir,
+                Bulk2Dir = Bulk2Dir,
+                ChrSizeThreshold = ChrSizeThreshold,
+                AnalysisChrNames = AnalysisChrNames,
+                MinMq = MinMq,
+                MinBq = MinBq,
+                AdjustMq = AdjustMq,
+                OutputDir = OutputDir,
+                ThreadNumber = ThreadNumber,
+                SnpEffMaxHeap = SnpEffMaxHeap,
+                SnpEffConfigFile = SnpEffConfigFile,
+                SnpEffDatabaseName = SnpEffDatabaseName
+            };
+        }
+    }
+}
---
+++ PolyploidQtlSeq/Options/Pipeline/VariantCallPipelineSettingValueDTO.cs
@@ -0,0 +1,57 @@
+﻿using PolyploidQtlSeqCore.MappingAndVariantCall;
+
+namespace PolyploidQtlSeq.Options.Pipeline
+{
+    internal class VariantCallPipelineSettingValueDTO : IVariantCallPipelineSettingValue
+    {
+        public VariantCallPipelineSettingValueDTO()
+        {
+            ReferenceSequence = "";
+            ThreadNumber = 0;
+            Parent1Dir = "";
+            Parent2Dir = "";
+            Bulk1Dir = "";
+            Bulk2Dir = "";
+            ChrSizeThreshold = 0;
+            AnalysisChrNames = "";
+            MinBq = 0;
+            MinMq = 0;
+            AdjustMq = 0;
+            OutputDir = "";
+            SnpEffMaxHeap = 0;
+            SnpEffConfigFile = "";
+            SnpEffDatabaseName = "";
+        }
+
+
+        public string ReferenceSequence { get; init; }
+
+        public int ThreadNumber { get; init; }
+
+        public string Parent1Dir { get; init; }
+
+        public string Parent2Dir { get; init; }
+
+        public string Bulk1Dir { get; init; }
+
+        public string Bulk2Dir { get; init; }
+
+        public int ChrSizeThreshold { get; init; }
+
+        public string AnalysisChrNames { get; init; }
+
+        public int MinMq { get; init; }
+
+        public int MinBq { get; init; }
+
+        public int AdjustMq { get; init; }
+
+        public string OutputDir { get; init; }
+
+        public int SnpEffMaxHeap { get; init; }
+
+        public string SnpEffConfigFile { get; init; }
+
+        public string SnpEffDatabaseName { get; init; }
+    }
+}
