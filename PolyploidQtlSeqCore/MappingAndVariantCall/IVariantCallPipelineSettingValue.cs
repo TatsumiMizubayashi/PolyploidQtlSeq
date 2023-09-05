5f5e21a74ee2f97c7266fb518e2667b796468d8d
TatsumiMizubayashi <112037683+TatsumiMizubayashi@users.noreply.github.com>
2023-09-05 10:37:12 +0900

    VariantCallPipelineSettings作成

---
+++ PolyploidQtlSeqCore/MappingAndVariantCall/IVariantCallPipelineSettingValue.cs
@@ -0,0 +1,12 @@
+﻿using PolyploidQtlSeqCore.Mapping;
+using PolyploidQtlSeqCore.VariantCall;
+
+namespace PolyploidQtlSeqCore.MappingAndVariantCall
+{
+    /// <summary>
+    /// 変異検出パイプライン設定値インターフェース
+    /// </summary>
+    interface IVariantCallPipelineSettingValue : IMappingSettingValue, IMappingSampleSettingValue, IVariantCallScenarioSettingValue
+    {
+    }
+}
---
+++ PolyploidQtlSeqCore/MappingAndVariantCall/VariantCallPipelineSettings.cs
@@ -0,0 +1,37 @@
+﻿using PolyploidQtlSeqCore.Mapping;
+using PolyploidQtlSeqCore.VariantCall;
+
+namespace PolyploidQtlSeqCore.MappingAndVariantCall
+{
+    /// <summary>
+    /// 変異検出パイプライン設定
+    /// </summary>
+    internal class VariantCallPipelineSettings
+    {
+        /// <summary>
+        /// 変異検出パイプライン設定インスタンスを作成する。
+        /// </summary>
+        /// <param name="settingValue">設定値</param>
+        public VariantCallPipelineSettings(IVariantCallPipelineSettingValue settingValue)
+        {
+            MappingSettings = new MappingSettings(settingValue);
+            MappingSampleSettings = new MappingSampleSettings(settingValue);
+            VariantCallScenarioSettings = new VariantCallScenarioSettings(settingValue);
+        }
+
+        /// <summary>
+        /// Mapping設定を取得する。
+        /// </summary>
+        public MappingSettings MappingSettings { get; }
+
+        /// <summary>
+        /// Mappingサンプル設定を取得する。
+        /// </summary>
+        public MappingSampleSettings MappingSampleSettings { get; }
+
+        /// <summary>
+        /// 変異検出シナリオ設定を取得する。
+        /// </summary>
+        public VariantCallScenarioSettings VariantCallScenarioSettings { get; }
+    }
+}
