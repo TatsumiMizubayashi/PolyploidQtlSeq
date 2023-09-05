7e641b161f98d4fc81afcc509fd9518e58d3ac54
TatsumiMizubayashi <112037683+TatsumiMizubayashi@users.noreply.github.com>
2023-09-05 09:08:21 +0900

    ChrSizeThreshold修正
    Obsolete属性を付与。
    引数１個のコンストラクタを追加。

--- PolyploidQtlSeqCore/QtlAnalysis/Chr/ChrSizeThreshold.cs
+++ PolyploidQtlSeqCore/QtlAnalysis/Chr/ChrSizeThreshold.cs
@@ -20,26 +20,31 @@ namespace PolyploidQtlSeqCore.QtlAnalysis.Chr
         /// <summary>
         /// しきい値の規定値
         /// </summary>
+        [Obsolete("削除予定")]
         public const int DEFAULT = 10_000_000;
 
         /// <summary>
         /// オプションスイッチのShortName
         /// </summary>
+        [Obsolete("削除予定")]
         public const string SHORT_NAME = "cs";
 
         /// <summary>
         /// オプションスイッチのLongName
         /// </summary>
+        [Obsolete("削除予定")]
         public const string LONG_NAME = "chrSize";
 
         /// <summary>
         /// オプションスイッチの説明
         /// </summary>
+        [Obsolete("削除予定")]
         public const string DESCRIPTION = "Threshold for length of chromosomes to be analyzed. Chromosomes with a length more than this value are analyzed.";
 
         /// <summary>
         /// データ検証エラーメッセージ
         /// </summary>
+        [Obsolete("削除予定")]
         public const string VALIDATION_ERROR_MESSAGE = "The -cs option must be an integer greater than 1 and less than or equal to 100,000,000.";
 
         /// <summary>
@@ -54,6 +59,17 @@ namespace PolyploidQtlSeqCore.QtlAnalysis.Chr
             Value = OptionValue.GetValue(LONG_NAME, threshold, parameterDictionary, userOptionDictionary);
 
             if (Value < MINIMUM || Value > MAXIMUM) throw new ArgumentException(VALIDATION_ERROR_MESSAGE);
+        }
+
+        /// <summary>
+        /// 染色体長のしきい値
+        /// </summary>
+        /// <param name="threshold">しきい値</param>
+        public ChrSizeThreshold(int threshold)
+        {
+            if (threshold < MINIMUM || threshold > MAXIMUM) throw new ArgumentOutOfRangeException(nameof(threshold));
+
+            Value = threshold;
         }
 
         /// <summary>
