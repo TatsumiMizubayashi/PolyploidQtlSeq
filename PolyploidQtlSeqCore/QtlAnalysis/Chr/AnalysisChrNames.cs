c58d285ef438dd8c735514950613643e5b9bc3db
TatsumiMizubayashi <112037683+TatsumiMizubayashi@users.noreply.github.com>
2023-09-05 09:10:19 +0900

    AnalysisChrNames修正
    Obsolete属性を付与。
    引数１個のコンストラクタを追加。

--- PolyploidQtlSeqCore/QtlAnalysis/Chr/AnalysisChrNames.cs
+++ PolyploidQtlSeqCore/QtlAnalysis/Chr/AnalysisChrNames.cs
@@ -10,16 +10,19 @@ namespace PolyploidQtlSeqCore.QtlAnalysis.Chr
         /// <summary>
         /// オプションスイッチのShortName
         /// </summary>
+        [Obsolete("削除予定")]
         public const string SHORT_NAME = "cn";
 
         /// <summary>
         /// オプションスイッチのLongName
         /// </summary>
+        [Obsolete("削除予定")]
         public const string LONG_NAME = "chrNames";
 
         /// <summary>
         /// オプションスイッチの説明
         /// </summary>
+        [Obsolete("削除予定")]
         public const string DESCRIPTION = "Specify the chromosome name to be analyzed. If there are more than one, separate them with commas. ";
 
         private static readonly char[] _delimiter = new[] { ',' };
@@ -34,6 +37,19 @@ namespace PolyploidQtlSeqCore.QtlAnalysis.Chr
             IReadOnlyDictionary<string, bool> userOptionDictionary)
         {
             Value = OptionValue.GetValue(LONG_NAME, value, parameterDictionary, userOptionDictionary);
+            Names = string.IsNullOrEmpty(Value)
+                ? Array.Empty<string>()
+                : Value.Split(_delimiter);
+            HasNames = Names.Any();
+        }
+
+        /// <summary>
+        /// 解析対象染色体名を作成する。
+        /// </summary>
+        /// <param name="value">カンマ区切り染色体名</param>
+        public AnalysisChrNames(string value)
+        {
+            Value = value;
             Names = string.IsNullOrEmpty(Value)
                 ? Array.Empty<string>()
                 : Value.Split(_delimiter);
