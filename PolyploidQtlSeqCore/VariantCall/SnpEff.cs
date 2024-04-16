5a48034fadf7e2ecfe2946a92b16b5f6dad1e229
TatsumiMizubayashi <112037683+TatsumiMizubayashi@users.noreply.github.com>
2024-04-16 11:07:15 +0900

    Edit SnpEff
    Fixed a problem that caused a process error when snpEff output a warning message.

--- PolyploidQtlSeqCore/VariantCall/SnpEff.cs
+++ PolyploidQtlSeqCore/VariantCall/SnpEff.cs
@@ -29,9 +29,41 @@ namespace PolyploidQtlSeqCore.VariantCall
         {
             if (!_option.CanSneEff) throw new ArgumentException("Unable to execute SnpEff.");
 
+            try
+            {
+                await RunSnpEffAsync(inputVcf, outputVcfFilePath);
+
+                return new VcfFile(outputVcfFilePath);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// snpEffコマンドを作成する。
+        /// </summary>
+        /// <param name="inputVcf">入力VCF</param>
+        /// <returns></returns>
+        private string CreateCommand(VcfFile inputVcf)
+        {
             var command = $"snpEff -Xms2g -Xmx{_option.MaxHeap.Value}g ";
             if (_option.ConfigFile.HasFile) command += $"-c {_option.ConfigFile.Path} ";
             command += $"{_option.Database.Value} {inputVcf.Path} -noStats";
+
+            return command;
+        }
+
+        /// <summary>
+        /// SnpEffを実行する。
+        /// </summary>
+        /// <param name="inputVcf">入力VCF</param>
+        /// <param name="outputVcfFilePath">出力VCFファイルPath</param>
+        /// <returns></returns>
+        private async ValueTask RunSnpEffAsync(VcfFile inputVcf, string outputVcfFilePath)
+        {
+            var command = CreateCommand(inputVcf);
             CommandLog.Add(command);
 
             try
@@ -41,12 +73,16 @@ namespace PolyploidQtlSeqCore.VariantCall
                 {
                     writer.WriteLine(line);
                 }
-
-                return new VcfFile(outputVcfFilePath);
             }
             catch (ProcessErrorException ex)
             {
-                Log.Add(ex.ToString());
+                Log.AddRange(ex.ErrorOutput);
+
+                // await foreach()はExitCodeが0以外か標準エラーがある場合例外を発生する。
+                // 処理はうまくいっているが警告がある場合標準エラーに追加され例外が発生してしまう。
+                // ExitCodeが0ならば問題ないと判断し終了する。
+                if (ex.ExitCode == 0) return;
+
                 throw;
             }
         }
